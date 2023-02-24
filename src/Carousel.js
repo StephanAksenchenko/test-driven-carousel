069f41179cf0ac74d67f3c4a09f38a4262ca0970
Aksenchenko Stefan <stefanaksencenko@gmail.com>
2023-02-24 13:59:57 +0600

    :recycle: Replace Carousel state with HasIndex HOC

--- src/Carousel.js
+++ src/Carousel.js
@@ -2,10 +2,15 @@ import PropTypes from 'prop-types';
 import React from 'react';
 import CarouselButton from './CarouselButton';
 import CarouselSlide from './CarouselSlide';
+import HasIndex from './HasIndex';
 
-class Carousel extends React.PureComponent {
+export class Carousel extends React.PureComponent {
   static propTypes = {
     defaultImg: CarouselSlide.propTypes.Img,
+    defaultImgHeight: CarouselSlide.propTypes.imgHeight,
+    slideIndex: PropTypes.number.isRequired,
+    slideIndexDecrement: PropTypes.func.isRequired,
+    slideIndexIncrement: PropTypes.func.isRequired,
     slides: PropTypes.arrayOf(PropTypes.shape(CarouselSlide.propTypes))
       .isRequired,
   };
@@ -21,28 +26,30 @@ class Carousel extends React.PureComponent {
   };
 
   prev = () => {
-    const { slides } = this.props;
-    this.setState(({ slideIndex }) => ({
-      slideIndex: (slideIndex + slides.length - 1) % slides.length,
-    }));
+    const { slideIndexDecrement, slides } = this.props;
+    slideIndexDecrement(slides.length);
   };
 
   next = () => {
-    const { slides } = this.props;
-    this.setState(({ slideIndex }) => ({
-      slideIndex: (slideIndex + 1) % slides.length,
-    }));
+    const { slideIndexIncrement, slides } = this.props;
+    slideIndexIncrement(slides.length);
   };
 
   render() {
-    const { defaultImg, defaultImgHeight, slides, ...rest } = this.props;
+    const {
+      defaultImg,
+      defaultImgHeight,
+      slideIndex,
+      slides,
+      ...rest
+    } = this.props;
 
     return (
       <div {...rest}>
         <CarouselSlide
           Img={defaultImg}
           imgHeight={defaultImgHeight}
-          {...slides[this.state.slideIndex]}
+          {...slides[slideIndex]}
         />
         <CarouselButton data-action="prev" onClick={this.prev}>
           Prev
@@ -55,4 +62,4 @@ class Carousel extends React.PureComponent {
   }
 }
 
-export default Carousel;
+export default HasIndex(Carousel, 'slideIndex');
