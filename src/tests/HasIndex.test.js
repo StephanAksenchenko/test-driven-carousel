c0b84a903292a1cfded54fd747a76133fda552eb
Aksenchenko Stefan <stefanaksencenko@gmail.com>
2023-02-25 15:13:00 +0600

    :sparkles: Allow the index in HasIndex to be controlled

--- src/tests/HasIndex.test.js
+++ src/tests/HasIndex.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import HasIndex from '../HasIndex';
 
-describe('HasIndex', () => {
+describe('HasIndex()', () => {
   const MockComponent = () => null;
   MockComponent.displayName = 'MockComponent';
   const MockComponentWithIndex = HasIndex(MockComponent, 'index');
@@ -12,13 +12,45 @@ describe('HasIndex', () => {
   });
 
   let wrapper;
-
   beforeEach(() => {
     wrapper = shallow(<MockComponentWithIndex />);
   });
 
-  it('has an initial `index` state of 0', () => {
+  it('has initial `index` state equal to the `defaultIndex` prop', () => {
+    // 1
     expect(wrapper.state('index')).toBe(0);
+    const wrapper2 = shallow(<MockComponentWithIndex defaultIndex={1} />);
+    expect(wrapper2.state('index')).toBe(1);
+  });
+
+  it('always has `index` state equal to the `index` prop', () => {
+    // 2
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />,
+    );
+    expect(wrapperWithInitialIndex.state('index')).toBe(1);
+    wrapper.setProps({ index: 2 });
+    expect(wrapper.state('index')).toBe(2);
+  });
+
+  it('allows `index` state to change if the `index` prop is unset', () => {
+    // 3
+    const wrapperWithInitialIndex = shallow(
+      <MockComponentWithIndex index={1} />,
+    );
+    wrapperWithInitialIndex.setProps({ index: undefined });
+    wrapperWithInitialIndex.setState({ index: 3 });
+    expect(wrapperWithInitialIndex.state('index')).toBe(3);
+  });
+
+  it('calls `onIndexChange` on decrement/increment', () => {
+    // 4
+    const onIndexChange = jest.fn();
+    wrapper.setProps({ index: 0, onIndexChange });
+    wrapper.prop('indexDecrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 2 } });
+    wrapper.prop('indexIncrement')(3);
+    expect(onIndexChange).toHaveBeenCalledWith({ target: { value: 1 } });
   });
 
   it('passes the `index` state down as the `index` prop', () => {
